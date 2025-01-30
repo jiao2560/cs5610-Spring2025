5d39c9abdb162ec965f28f45d3da38ffd27e6509
Wenbo Jiao <jiao.wenbo@northeastern.edu>
2025-01-30 12:02:54 -0800

    add loops for/while and do/while

---
+++ week4/js/script.js
@@ -0,0 +1,13 @@
+// Using a for loop to log powers of 2 from 2 to 1024
+console.log("Powers of 2 using a for loop:");
+for (let i = 2; i <= 1024; i *= 2) {
+    console.log(i);
+}
+
+// Using a while loop to log powers of 2 from 2 to 1024
+console.log("Powers of 2 using a while loop:");
+let num = 2;
+while (num <= 1024) {
+    console.log(num);
+    num *= 2;
+}

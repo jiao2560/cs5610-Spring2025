6e02add5eccb3bc0f325d824e8586803517fc299
Wenbo Jiao <jiao.wenbo@northeastern.edu>
2025-02-13 11:32:11 -0800

    Activity - Node.js Core Module – File system (fs) completed

---
+++ week6/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const filePath = 'message.txt';
+const message = 'Hello, this is a test message written to a file!';
+
+// Write the message to a new file
+fs.writeFile(filePath, message, (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+        return;
+    }
+    console.log('Message successfully written to file.');
+
+    // Read the message back
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        console.log('Read from file:', data);
+    });
+});

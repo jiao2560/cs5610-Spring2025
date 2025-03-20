062caf0036b411080135fdd69421010dd0b2ca65
Wenbo Jiao <jiao.wenbo@northeastern.edu>
2025-03-20 11:34:34 -0700

    Added AddTask form: - Created AddTask.jsx component - Added form with labeled inputs and submit button - Styled the form using index.css - Rendered AddTask component in App.jsx

--- week9/my-react-app/src/App.jsx
+++ week9/my-react-app/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import TasksList from "./components/TasksList"; // Import TasksList component
+import AddTask from "./components/AddTask"; // Import AddTask component
 
 export default function App() {
   const appName = "My Awesome App";
   const version = 2;
 
-
   return (
     <div className="app-container">
       <Header myAppName={appName} version={version} />
-      <TasksList /> {/* Use the component */}
+      <AddTask /> {/* Render AddTask form below Header */}
+      <TasksList /> {/* Render the task list */}
     </div>
   );
 }

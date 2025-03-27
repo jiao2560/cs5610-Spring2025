4d31be5977238ce0832d94d9210fef1f45fb1afe
Wenbo Jiao <jiao.wenbo@northeastern.edu>
2025-03-27 12:45:24 -0700

    make  React Router available for our app

--- week9/my-react-app/src/main.jsx
+++ week9/my-react-app/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Use 'react-router-dom' not 'react-router'
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter> {/* ✅ Wrap App inside BrowserRouter */}
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);

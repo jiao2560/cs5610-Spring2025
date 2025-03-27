bd56040d930e16272f1b6ed0cea2f866ebb4e78b
Wenbo Jiao <jiao.wenbo@northeastern.edu>
2025-03-27 13:11:13 -0700

    add required data to db

--- week9/my-react-app/src/main.jsx
+++ week9/my-react-app/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Use 'react-router-dom' not 'react-router'
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter> {/* ✅ Wrap App inside BrowserRouter */}
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
